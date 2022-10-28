a79ab0bd5768f5575e03429247ccc4f38132a744
koushiksarkerDIU <antusarker411@gmail.com>
2022-10-28 15:34:47 +0600

    quiz done

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -3,8 +3,8 @@ import "./Option.css";
 
 const Option = ({ option }) => {
   return (
-    <div className="w-3/4 m-2 option">
-      <h2 className="p-2"> {option}</h2>
+    <div className="w-3/4 m-2 p-3 option">
+      <button>{option}</button>
     </div>
   );
 };
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -5,8 +5,10 @@ const Question = (props) => {
   //   console.log(props.question);
   const { question, options, correctAnswer } = props.question;
   return (
-    <div className="border p-10 w-3/5">
-      <h2>Quiz :{question}</h2>
+    <div className="rounded-lg p-5 mb-20 w-3/5 shadow-xl">
+      <div>
+        <h2>Quiz :{question}</h2>
+      </div>
       <div className="grid grid-cols-2 justify-items-center justify-center p-2">
         {options.map((option, i) => (
           <Option key={i} option={option}></Option>
